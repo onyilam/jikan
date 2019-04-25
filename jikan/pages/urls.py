02c497c999339f24d805f46523d283454c4af9d2
olam <onyilam@gmail.com>
2019-04-25 15:01:38 -0400

    fixed header urls

--- jikan/pages/urls.py
+++ jikan/pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
