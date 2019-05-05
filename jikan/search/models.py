023909ed2f50532927099257197903bd63a217ef
olam <onyilam@gmail.com>
2019-05-05 14:38:10 -0400

    table to store academic papers

--- jikan/search/models.py
+++ jikan/search/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Paper(models.Model):
+#   store the academic paper information
+    title = models.CharField(max_length=1000)
+    venue = models.CharField(max_length=200)
+    year = models.IntegerField()
+    abstract = models.CharField(max_length = 10000)
+    authors = models.CharField(max_length = 1000)
+    reference = models.ForeignKey('self', on_delete=models.CASCADE)
+
+
