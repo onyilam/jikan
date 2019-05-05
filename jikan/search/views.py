dfa85b5adf0beececf49c123fe8b8c5f42f64a52
olam <onyilam@gmail.com>
2019-05-05 14:39:22 -0400

    refractor

--- jikan/search/views.py
+++ jikan/search/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Paper
 
-# Create your views here.
+def searchpaper(request):
+    if request.method == 'GET':
+        query = request.GET.get('q')
+
+        submitbutton= request.GET.get('submit')
+
+        if query is not None:
+            lookups= Q(title__icontains=query) | Q(content__icontains=query)
+
+            results= Paper.objects.filter(lookups).distinct()
+
+            context={'results': results,
+                     'submitbutton': submitbutton}
+
+            return render(request, 'search/search.html', context)
+
+        else:
+            return render(request, 'search/search.html')
+
+    else:
+        return render(request, 'search/search.html')
