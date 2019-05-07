4692100d7eb92baa8b5d781345719342a6cb0d60
olam <onyilam@gmail.com>
2019-05-07 09:41:34 -0400

    search template

--- jikan/search/models.py
+++ jikan/search/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class Paper(models.Model):
 #   store the academic paper information
+    aminer_id = models.CharField(max_length=100, null=True)
     title = models.CharField(max_length=1000)
-    venue = models.CharField(max_length=200)
-    year = models.IntegerField()
-    abstract = models.CharField(max_length = 10000)
-    authors = models.CharField(max_length = 1000)
-    reference = models.ForeignKey('self', on_delete=models.CASCADE)
+    venue = models.CharField(max_length=200, null=True)
+    year = models.IntegerField(null=True)
+    abstract = models.CharField(max_length = 10000, null=True)
+    authors = models.CharField(max_length = 1000, null=True)
+    reference = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
 
 
