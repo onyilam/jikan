beb0b0a9ae831ee191df35f12ad2abf393d2198c
olam <onyilam@gmail.com>
2019-05-13 14:47:12 -0400

    no longer including posts feature

--- jikan/posts/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from .views import HomePageView, CreatePostView # new
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('post/', CreatePostView.as_view(), name='add_post') # new
-]
