d2ff2527e2941f1b257e227f45b5ef8e799ba9bc
olam <onyilam@gmail.com>
2019-05-20 13:00:31 -0400

    remove duplicated code

--- jikan/load_citation_data.py
+++ jikan/load_citation_data.py
@@ -7,44 +7,30 @@ from ast import literal_eval
 from search.models import Paper, Journal, Author
 
 
-def save_paper_from_row_if_existed(paper_row):
-    paper, _ = Paper.objects.get_or_create(aminer_id=paper_row['id'])
-    paper.aminer_id = paper_row['id']
-    paper.title = paper_row['title']
-    paper.abstract = paper_row['abstract']
-    journal, _ = Journal.objects.get_or_create(name=paper_row['venue'])
-    paper.journal = journal
-    paper.year = paper_row['year']
-    paper.n_citation = paper_row['n_citation']
-    paper.save()
+def save_paper_from_row(paper_row):
+    try:
+        p = Paper.objects.get(aminer_id=paper_row['id'])
+        if p:
+            print('Paper Already Existed')
+    except Paper.DoesNotExist:
+        paper, _ = Paper.objects.get_or_create(aminer_id=paper_row['id'])
+        print("Import New Paper", paper_row['id'])
+        paper.aminer_id = paper_row['id']
+        paper.title = paper_row['title']
+        paper.abstract = paper_row['abstract']
+        journal, _ = Journal.objects.get_or_create(name=paper_row['venue'])
+        paper.journal = journal
+        paper.year = paper_row['year']
+        paper.n_citation = paper_row['n_citation']
+        paper.save()
 
-    for author in literal_eval(paper_row['authors']):
-        fname = author.split(" ")[0]
-        lname = author.split(" ")[-1]
-        author, _ = Author.objects.get_or_create(first_name=fname, last_name=lname)
-        paper.authors.add(author)
+        for author in literal_eval(paper_row['authors']):
+            fname = author.split(" ")[0]
+            lname = author.split(" ")[-1]
+            author, _ = Author.objects.get_or_create(first_name=fname, last_name=lname)
+            paper.authors.add(author)
 
-    paper.save()
-
-def save_paper_from_row_newfile(paper_row):
-    paper = Paper()
-    paper.aminer_id = paper_row['id']
-    paper.title = paper_row['title']
-    paper.abstract = paper_row['abstract']
-    journal, _ = Journal.objects.get_or_create(name=paper_row['venue'])
-    paper.journal = journal
-    paper.year = paper_row['year']
-    paper.n_citation = paper_row['n_citation']
-    paper.references = paper_row['references']
-    paper.save()
-
-    for author in literal_eval(paper_row['authors']):
-        fname = author.split(" ")[0]
-        lname = author.split(" ")[-1]
-        author, _ = Author.objects.get_or_create(first_name=fname, last_name=lname)
-        paper.authors.add(author)
-
-    paper.save()
+        paper.save()
 
 
 # the main function for the script, called by the shell
@@ -56,8 +42,10 @@ if __name__ == "__main__":
         papers_df = pd.read_csv(sys.argv[1])
 
         # apply save_review_from_row to each review in the data frame
+        papers_df = papers_df[250000:]
+        print(len(papers_df))
         papers_df.apply(
-            save_paper_from_row_if_existed,
+            save_paper_from_row,
             axis=1
         )
 
