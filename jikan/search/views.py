f039aa601d112b2001508b315312f92bcae2589b
olam <onyilam@gmail.com>
2019-05-25 10:41:57 -0400

    paginate results

--- jikan/search/views.py
+++ jikan/search/views.py
@@ -2,22 +2,32 @@
 from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from .models import Paper, Preference
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 
-
-
 def searchpaper(request):
     if request.method == 'GET':
         query = request.GET.get('q')
         submitbutton= request.GET.get('submit')
+        page = request.GET.get('page', 1)
+
         if query is not None:
             lookups = Q(title__icontains=query)
             results = Paper.objects.filter(lookups).distinct().order_by('-likes', '-n_citation')
+            paginator = Paginator(results, 20)
+            try:
+                numbers = paginator.page(page)
+            except PageNotAnInteger:
+                numbers = paginator.page(1)
+            except EmptyPage:
+                numbers = paginator.page(paginator.num_pages)
+
             context = {'results': results,
-                      'submitbutton': submitbutton}
+                       'submitbutton': submitbutton,
+                       'numbers': numbers}
             return render(request, 'home.html', context)
         else:
             return render(request, 'home.html')
