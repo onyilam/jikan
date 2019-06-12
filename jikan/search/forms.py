9ea952122b01e13e18e013b3a021bc5f8b120449
Onyi <onyilam@gmail.com>
2019-06-12 13:03:44 +0800

    simplied form

--- jikan/search/forms.py
+++ jikan/search/forms.py
@@ -11,16 +11,17 @@ class CommentForm(ModelForm):
 
 
 class PaperForm(ModelForm):
-    journal = ModelChoiceField(
-        queryset = Paper.objects.all(),
-        empty_label = 'Working Paper',
-        label = 'Venue',
-        help_text='The venue the paper appears',
-        widget = ModelSelect2(url='journal_autocomplete')
-    )
-    c =[("1", "Early development"), ("2", "Writing and Editing"), ("3", "Subitted"), ("4", "R&R"), ("5", "In Print")]
+    # journal = ModelChoiceField(
+    #     queryset = Paper.objects.all(),
+    #     empty_label = 'Working Paper',
+    #     label = 'Venue',
+    #     help_text='The venue the paper appears',
+    #     widget = ModelSelect2(url='journal_autocomplete')
+    # )
+    c =[("1", "Early development"), ("2", "Writing and Editing"),
+    ("3", "Subitted"), ("4", "R&R"), ("5", "In Print")]
     status = forms.ChoiceField(choices=c, label="Status")
 
     class Meta:
         model = Paper
-        fields = ('title', 'abstract', 'status', 'journal')
+        fields = ('title', 'abstract', 'status')
