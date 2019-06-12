0c10d7161579e17599627e4826aa8d12288d74d8
Onyi <onyilam@gmail.com>
2019-06-12 12:11:56 +0800

    added status field

--- jikan/search/forms.py
+++ jikan/search/forms.py
@@ -1,7 +1,8 @@
 from .models import Paper, Comment, Author
-from django.forms import ModelForm, ModelChoiceField
+from django.forms import ModelForm, ModelChoiceField, MultipleChoiceField
 from django import forms
 from dal.autocomplete import ModelSelect2
+#from floppyforms.widgets import input
 
 class CommentForm(ModelForm):
     class Meta:
@@ -12,14 +13,14 @@ class CommentForm(ModelForm):
 class PaperForm(ModelForm):
     journal = ModelChoiceField(
         queryset = Paper.objects.all(),
+        empty_label = 'Working Paper',
+        label = 'Venue',
+        help_text='The venue the paper appears',
         widget = ModelSelect2(url='journal_autocomplete')
     )
-
-    authors = ModelChoiceField(
-        queryset = Author.objects.all(),
-        widget = ModelSelect2(url='author_autocomplete')
-    )
+    c =[("1", "Early development"), ("2", "Writing and Editing"), ("3", "Subitted"), ("4", "R&R"), ("5", "In Print")]
+    status = forms.ChoiceField(choices=c, label="Status")
 
     class Meta:
         model = Paper
-        fields = ('title', 'authors', 'abstract', 'journal')
+        fields = ('title', 'abstract', 'status', 'journal')
