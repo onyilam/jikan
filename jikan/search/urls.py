9f7ca86c472b6ae65168b9182b35d7acd416b9b7
Onyi Lam <onyilam@gmail.com>
2019-07-01 13:16:08 -0400

    load user form

--- jikan/search/urls.py
+++ jikan/search/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
      url(r'^load/', views.load_paper, name='load_paper'),
      url(r'^edit/(?P<pk>\d+)/', views.edit_paper, name='edit_paper'),
      url(r'^load_user/', views.load_user, name='load_user'),
+     url(r'^edit_user/(?P<pk>\d+)/', views.edit_user, name='edit_user'),
      url(r'^ajax_calls/search/', views.autocompletePaper),
      path('profile/<int:pk>/', views.view_user, name="view_user"),
      path('', views.HomePageView.as_view(), name='home'),
